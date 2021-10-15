92940790d775e8e3973f88058dc8f2db89fc9438
Felix Obenhuber <felix@obenhuber.de>
2021-10-15 17:02:30 +0200

    cp

--- examples/01_panic.rs
+++ examples/01_panic.rs
@@ -1,4 +1,9 @@
 fn main() {
     std::env::set_var("RUST_BACKTRACE", "1");
+
+    assert_eq!(1, 2);
+    debug_assert!(false);
     panic!("ohhh no");
+    todo!("here is something missing");
+    unimplemented!("here needs something to be done");
 }
